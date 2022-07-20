414a83bf32af0c687a1c9264e2be8cfa5c1e0210
Felipe Ferraresi <felipe1181jua@gmail.com>
2022-07-19 22:48:47 -0300

    feat: create theme provider

--- src/ProvidersConfig.tsx
+++ src/ProvidersConfig.tsx
@@ -1,12 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from 'core/routes';
+import {dark} from 'core/styles/themes';
 import React from 'react';
+import {ThemeProvider} from 'styled-components/native';
 
 const ProvidersConfig: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <ThemeProvider theme={dark}>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
